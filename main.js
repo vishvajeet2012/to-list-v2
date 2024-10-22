e38ae22ee762dd95f683dc08c57d1943d0790186
vishvajeet shukla <djritikshukla@gmail.com>
2024-10-22 15:56:20 +0530

    update

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 console.log("this is vishu ..")
-
+let taskStore = [] //array
 
 
 function getTask () {
-let inputBox  = document.getElementById("inputGet").value   // here  i get value form input get
-let taskSection = document.getElementById("taskSection") // task section where  paragraoh tag will append
-let para =  document.createElement('p') // cretae paragraph tag
-let deleteBtn = document.createElement('button') // delete btn for paragraph tag 
+
+    let inputBox  = document.getElementById("inputGet").value   // here  i get value form input get
+    let taskSection = document.getElementById("taskSection") // task section where  paragraoh tag will append
+    let para =  document.createElement('p') // cretae paragraph tag
+    let deleteBtn = document.createElement('button') // delete btn for paragraph tag 
 
         taskSection.appendChild(para)   
         taskSection.append(deleteBtn)
         deleteBtn.innerText ="Delete"
     para.innerText = inputBox
-
+taskStore.push(inputBox)
 
  deleteBtn.addEventListener("click" , function(){
-        
+       console.log(taskStore)
 
 })
 
